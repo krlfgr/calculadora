014959cfd1d1cfcfb7f107cba7cfa3dcdd1ed92a
krlfgr <karoldhn05@gmail.com>
2025-04-30 20:06:31 -0500

    nuevas rutas y html

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from livereload import Server
+from operaciones import sumar
+from operaciones import division_piso
 
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "pagina de inicio"
+    return render_template("home.html")
+
+@app.route("/suma")
+def ruta_suma():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "Faltan datos"
+    return f"El resultado de la suma es {sumar(numero1,numero2)}"
+
+@app.route("/division_piso")
+def ruta_division_piso():
+    numero1=request.args.get("numero1",type=float)
+    numero2=request.args.get("numero2",type=float)
+    if numero1 is None or numero2 is None:
+        return "Faltan datos"
+    return f"El resultado de la division piso es {division_piso(numero1,numero2)}"
+
+if __name__ == "__main__":
+    server = Server(app.wsgi_app)
+    server.serve()
---
+++ operaciones.py
@@ -0,0 +1,5 @@
+def sumar(a,b):
+    return a+b
+
+def division_piso(a,b):
+    return a//b
